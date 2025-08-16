0471a22902fb3af99a78720317442004394f7518
KuyaMaki <benitomarkjosh07@gmail.com>
2025-08-16 22:29:13 +0800

    Hello World

--- index.php
+++ index.php
@@ -12,6 +12,12 @@
     <div class="container mx-auto p-4">
         <h1 class="text-3xl font-bold mb-4 text-red-500">Welcome to My Website</h1>
         <p class="text-gray-700">This is a simple page styled with Tailwind CSS.</p>
+        <h1>Hello
+        </h1>
+        <h1 class="text-3xl font-bold mb-4 text-yellow-500">Welcome to My Website</h1>
+        <p class="text-gray-700">This is a simple page styled with Tailwind CSS.</p>
+        
+
     </div>
 </body>
 
