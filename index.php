32288b3caea01b3e1ddef9cf691fc0b8de128b56
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2025-08-18 23:15:33 +0800

    18/08/2025 DD/MM//YYYY -Jenrick Aran

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
         </h1>
         <h1 class="text-3xl font-bold mb-4 text-yellow-500">Welcome to My Website</h1>
         <p class="text-gray-700">This is a simple page styled with Tailwind CSS.</p>
-        
     </div>
 </body>
 
